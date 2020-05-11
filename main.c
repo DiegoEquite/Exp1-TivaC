314314ffb7e923729ccffde8077addd6147c4a8c
DiegoEquite <60586329+DiegoEquite@users.noreply.github.com>
2020-05-11 13:16:01 -0600

    configuraciones

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "inc/tmc123gh6pm.h"
+
+/**
+ * main.c
+ */
+#include "driverlib/gpio.h"
+
+int main(void)
+{
+	return 0;
+}
